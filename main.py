7d2849b28426d6a0b18e648f8a303e9b080247f2
Alperosci <alperosci@gmail.com>
2025-06-01 18:37:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def install_package(command):
     print(f"Installing with: {command}")
     os.system(command)
 
+typeins = int(input("Debian based(1) or Arch based(2) > "))
+
 print("""
 What do you want to install
 -office-
@@ -49,8 +51,30 @@ pacman_packages = {
     13: "sudo pacman -S retroarch"
 }
 
-if prompt in pacman_packages:
-    install_package(pacman_packages[prompt])
-else:
-    print("Invalid selection, please choose a number from the list.")
+apt_packages = {
+    1: "sudo apt install libreoffice-fresh",
+    2: "sudo apt install gimp",
+    3: "sudo apt install firefox",
+    4: "sudo apt install chromium",
+    5: "sudo apt install thunderbird",
+    6: "sudo apt install filezilla",
+    7: "sudo apt install vlc",
+    8: "sudo apt install audacity",
+    9: "sudo apt install kdenlive",
+    10: "sudo apt install code",
+    11: "sudo apt install neovim",
+    12: "sudo apt install steam",
+    13: "sudo apt install retroarch"
+}
+
+if typeins == 2:
+    if prompt in pacman_packages:
+        install_package(pacman_packages[prompt])
+    else:
+        print("Invalid selection, please choose a number from the list.")
+elif typeins == 1:
+    if prompt in apt_packages:
+        install_package(apt_packages[prompt])
+    else:
+        print("Invalid selection, please choose a number from the list.")
 
