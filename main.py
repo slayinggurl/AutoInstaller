bc0548cc30fc3172e995d09c7cf9e911b40a87ed
Alperosci <alperosci@gmail.com>
2025-06-03 20:12:01 +0300

    added OBS

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ What do you want to install (write the category if you want install all packages
 7- VLC
 8- Audacity
 9- Kdenlive
+14- OBS
 -programming-
 10- VScode (code)
 11- Nvim
@@ -45,10 +46,11 @@ pacman_packages = {
     "11": "sudo pacman -S neovim",
     "12": "sudo pacman -S steam",
     "13": "sudo pacman -S retroarch",
+    "14": "sudo pacman -S obs-studio",
     "office": "sudo pacman -S libreoffice-fresh gimp",
     "web": "sudo pacman -S firefox chromium",
     "communication": "sudo pacman -S thunderbird filezilla",
-    "multimedia": "sudo pacman -S vlc audacity kdenlive",
+    "multimedia": "sudo pacman -S vlc audacity kdenlive obs-studio",
     "programming": "sudo pacman -S code neovim",
     "gaming": "sudo pacman -S steam retroarch"
 }
@@ -67,10 +69,11 @@ apt_packages = {
     "11": "sudo apt install neovim",
     "12": "sudo apt install steam",
     "13": "sudo apt install retroarch",
+    "14": "sudo apt install obs-studio",
     "office": "sudo apt install libreoffice-fresh gimp",
     "web": "sudo apt install firefox chromium",
     "communication": "sudo apt install thunderbird filezilla",
-    "multimedia": "sudo apt install vlc audacity kdenlive",
+    "multimedia": "sudo apt install vlc audacity kdenlive obs-studio",
     "programming": "sudo apt install code neovim",
     "gaming": "sudo apt install steam retroarch"
 }
@@ -79,10 +82,10 @@ if typeins == 2:
     if prompt in pacman_packages:
         install_package(pacman_packages[prompt])
     else:
-        print("Invalid selection, please choose a number from the list.")
+        print("Invalid selection, please choose a ID from the list.")
 elif typeins == 1:
     if prompt in apt_packages:
         install_package(apt_packages[prompt])
     else:
-        print("Invalid selection, please choose a number from the list.")
+        print("Invalid selection, please choose a ID from the list.")
 
